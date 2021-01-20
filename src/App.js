3cf81e8e6ee085dbb7f6dee61a04a1448a178e67
Phillip Aelony <phillael@yahoo.com>
2021-01-19 19:57:17 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import recipeStyles from "./recipe.module.scss";
+const {
+  main,
+  btn,
+  recipe,
+  instructions,
+  ingredientList,
+  openList,
+} = recipeStyles;
 
-function App() {
+const App = () => {
+  const [meal, setMeal] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const apiUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+  const getMeal = async () => {
+    const response = await fetch(apiUrl);
+    const foodData = await response.json();
+    const mealData = foodData.meals[0];
+    await setMeal(mealData);
+    getIngredients(mealData);
+
+    console.log(meal);
+  };
+
+  const getIngredients = (meal) => {
+    const mealIngredients = [];
+
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        mealIngredients.push(
+          meal[`strIngredient${i}`] + " - " + meal[`strMeasure${i}`]
+        );
+      } else {
+        break;
+      }
+    }
+    setIngredients(mealIngredients);
+    // console.log(mealIngredients);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={main}>
+      <header>
+        <h1>Let's cook something!</h1>
+        <button className={btn} onClick={getMeal}>
+          Get Food!
+        </button>
       </header>
+
+      {meal && (
+        <div className={recipe}>
+          <h2>OMG!!! {meal.strMeal}</h2>
+
+          <div>
+            <img src={meal.strMealThumb} alt="here is your meal!" />
+            <div className={instructions}>
+              <p>{meal.strInstructions}</p>
+            </div>
+          </div>
+          <button onClick={() => setOpen((prev) => !prev)} className={btn}>
+            See ingredient list!
+          </button>
+
+          <div className={`${ingredientList} ${open && openList}`}>
+            <ul>
+              {ingredients.map((ingredient) => {
+                return <li key={Math.random()}>{ingredient}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+      <footer>
+        <p>Copyright Phill Aelony 2021</p>
+        <a href="https://www.phillcodes.com">www🍤phillcodes🍤com</a>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
